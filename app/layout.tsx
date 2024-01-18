dfd85cfa18ecf62c78500596eda3696b1f2e19db
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-01-18 15:24:06 -0800

    updating main production

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="site-layout">
-          <Header />
-          {children}
+        <div className="site-layout text-center mt-5">
+          {/* <Header />
+          {children} */}
+          Work In Progress... 
         </div>
 
       </body>
