22c79ea0186153b82e3688cb0ef8abafbf348606
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-01-17 18:03:59 -0800

    updating sounds page to include my dc mixes

--- app/sounds/page.tsx
+++ app/sounds/page.tsx
@@ -18,10 +18,14 @@ const Events = () => {
                 <Link href="/sounds/events" className="objects_link focus:bg-black focus:text-white hover:bg-black hover:text-white">Events</Link>
             </div>
             <div className="site-section">
-                <iframe className="soundcloud_link" width="100%" height="100" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1692501831&color=%233100ff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
                 <div>
                     <a className="soundcloud_title" href="https://soundcloud.com/pomotect" title="pomotect" target="_blank">SPACE SOUNDS [1 of ?]</a>
                 </div>
+                <iframe className="soundcloud_link" width="100%" height="250" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1692501831&color=%233100ff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+                <div className="mt-10">
+                    <a className="soundcloud_title" href="https://soundcloud.com/stevie-hu/sets/zeba-residency" title="stevie-residency" target="_blank">STEVIE HU DC RESIDENCY</a>
+                </div>
+                <iframe className="soundcloud_link" width="100%" height="250" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1601725162&color=%230000ff&auto_play=false&hide_related=false&show_comments=true&show_user=false&show_reposts=false&show_teaser=false&visual=true"></iframe>
             </div>
         </div>
     )
