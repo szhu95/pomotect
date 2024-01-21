b081164b0c6ab85336b2e6e6adc37a007f5c3394
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-01-20 18:04:08 -0800

    updating the events page

--- app/sounds/events/page.tsx
+++ app/sounds/events/page.tsx
@@ -2,11 +2,13 @@ import { CustomButton } from '@/components';
 import { formatDate } from '@/utils';
 import Link from 'next/link';
 import React from 'react'
+import WinstonFlyer from '../../../assets/images/flyer-1.jpeg'
+import Image from 'next/image';
 
 const Events = () => {
 
     let lastUpdatedDate = formatDate();
-    
+
     return (
         <div>
             <div className="site-section">
@@ -17,8 +19,26 @@ const Events = () => {
                 <Link href="/sounds" scroll={false} className="objects_link focus:bg-black focus:text-white hover:bg-black hover:text-white">Mixes</Link>
                 <Link href="/sounds/events" scroll={false} className="objects_link bg-black text-white hover:bg-black hover:text-white">Events</Link>
             </div>
-
-            <div className="site-section">Events</div>
+            <div className="flex p-2 w-1/8 max-w-12">
+                <ol className="relative">
+                    <li>
+                        <span>Out of the Blue</span>
+                        <br></br>
+                        <span>Winston House, Los Angeles</span>
+                    </li>
+                    <div className="w-1/4 p-1 max-w-10 flex-col items-center center centered fixed">
+                        <figure className="portrait">
+                            <Image
+                                loading="eager"
+                                width={2831}
+                                height={2877}
+                                src={WinstonFlyer}
+                                alt="Altered Egos"
+                            />
+                        </figure>
+                    </div>
+                </ol>
+            </div>
         </div>
     )
 }
