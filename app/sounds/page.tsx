2e5be44c0f570c3d9ad672a5880b129ae7a65c47
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-01-20 16:07:56 -0800

    updating sounds styling

--- app/sounds/page.tsx
+++ app/sounds/page.tsx
@@ -1,4 +1,5 @@
 import { Soundcloud } from '@/components';
+import ScrollToTopButton from '@/components/ScrollToTopButton';
 import { formatDate } from '@/utils';
 import Link from 'next/link';
 import React from 'react';
@@ -18,8 +19,11 @@ const Events = () => {
                 <Link href="/sounds" className="objects_link bg-black text-white hover:bg-black hover:text-white">Mixes</Link>
                 <Link href="/sounds/events" className="objects_link focus:bg-black focus:text-white hover:bg-black hover:text-white">Events</Link>
             </div>
+            
             <Soundcloud title_href="https://soundcloud.com/pomotect" title="pomotect" label="SPACE SOUNDS [1 of ?]" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1692501831&color=%233100ff&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" />
             <Soundcloud title_href="https://soundcloud.com/stevie-hu/sets/zeba-residency" title="stevie-residency" label="STEVIE HU DC RESIDENCY" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1601725162&color=%230000ff&auto_play=false&hide_related=false&show_comments=true&show_user=false&show_reposts=false&show_teaser=false&visual=true" />
+
+            <ScrollToTopButton />
         </div>
     )
 }
--- app/words/page.tsx
+++ app/words/page.tsx
@@ -56,7 +56,7 @@ const Words = async () => {
           </div>
         )
       })}
-
+      
       <ScrollToTopButton />
     </div>
   )
