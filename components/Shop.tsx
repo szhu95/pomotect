bc22bea9f879523f94e77d101db98b5e2c13bbee
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-01-31 20:03:34 -0800

    adding new row layout for mobile

--- components/Shop.tsx
+++ components/Shop.tsx
@@ -9,7 +9,7 @@ export default function Shop({ response }: any) {
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
 
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
           {response.products.edges.map((item: any) => {
             let product = item.node;
             let image = product.images.edges[0].node;
