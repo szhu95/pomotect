35468815ddc2ada9c9d7940057bdfcb9458cbf17
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-01-31 16:06:49 -0800

    adding routing to menu for mobile navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,11 @@ import homeLogo from '../assets/images/home-logo.png'
 
 const Header = () => {
     const [cartFilled, setCartFilled] = useState(false)
+    const [menuIcon, setIcon] = useState(false);
+
+    const handleSmallerScreensNavigation = () => {
+        setIcon(!menuIcon);
+    }
 
     useEffect(() => {
         let checkout = typeof window !== "undefined" ? localStorage.getItem("checkoutId") : null;
@@ -13,10 +18,6 @@ const Header = () => {
             setCartFilled(true);
         }
     }, [])
-
-    function handleSmallScreenNavigation(event: MouseEvent<HTMLDivElement, MouseEvent>): void {
-        throw new Error('Function not implemented.');
-    }
 
     return (
         <div className="padding-y">
@@ -30,16 +31,42 @@ const Header = () => {
                     />
                 </Link>
             </div>
-            <div className="header-link hidden sm:block">
+            <div className="header-link hidden md:block">
                 <Link href="/about" scroll={false} className=" hover:bg-primary-blue px-2 hover:text-white">About</Link>
                 <Link href="/objects" scroll={false} className="margin-x px-2 hover:bg-primary-blue hover:text-white">Objects</Link>
                 <Link href="/words" scroll={false} className="margin-x px-2 hover:bg-primary-blue hover:text-white">Words</Link>
                 <Link href="/sounds" scroll={false} className="margin-x px-2 hover:bg-primary-blue hover:text-white">Sounds</Link>
                 <Link href="/cart" scroll={false} id="checkout-btn" className="float-right px-2 mr-2 hover:bg-terracotta hover:text-white">{cartFilled ? "Cart (!)" : "Cart"}</Link>
             </div>
-            <div onClick={handleSmallScreenNavigation} className="header-link block sm:hidden">
-                Menu
+            <div onClick={handleSmallerScreensNavigation} className="header-link block md:hidden cursor-pointer">
+                {menuIcon ? "X" : "Menu"}
                 <Link href="/cart" scroll={false} id="checkout-btn" className="float-right px-2 mr-2 hover:bg-terracotta hover:text-white">{cartFilled ? "Cart (!)" : "Cart"}</Link>
+            </div>
+
+            <div className={menuIcon ?
+                'md:hidden absolute top-[150px] right-0 bottom-0 italic left-0 flex justify-center items-center w-full h-screen bg-primary-blue text-center text-white ease-in duration-300'
+                :
+                'md:hidden absolute top-[150px] right-0 italic left-[-100%] flex justify-center items-center w-full h-screen bg-primary-blue text-white text-center ease-in duration-300'
+            }>
+                <div className="w-full">
+                    <ul>
+                        <li onClick={handleSmallerScreensNavigation} className="py-5 cursor-pointer text-white font-semibold">
+                            <Link href="/about" scroll={false} className=" hover:bg-primary-blue px-2">About</Link>
+                        </li>
+
+                        <li onClick={handleSmallerScreensNavigation} className="py-5 cursor-pointer text-white font-semibold">
+                            <Link href="/objects" scroll={false} className=" hover:bg-primary-blue px-2">Objects</Link>
+                        </li>
+
+                        <li onClick={handleSmallerScreensNavigation} className="py-5 cursor-pointer text-white font-semibold">
+                            <Link href="/words" scroll={false} className=" hover:bg-primary-blue px-2">Words</Link>
+                        </li>
+
+                        <li onClick={handleSmallerScreensNavigation} className="py-5 cursor-pointer text-white font-semibold">
+                            <Link href="/sounds" scroll={false} className=" hover:bg-primary-blue px-2">Sounds</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
