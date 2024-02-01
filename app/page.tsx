552e3b3eaec6de8e43baedea100a7e6bdbb95d87
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-02-01 11:59:03 -0800

    adding scroll to top button for splash page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 import { Hero } from '@/components'
+import ScrollToTopButton from '@/components/ScrollToTopButton'
 
 export default function Home() {
 
   return (
     <main className="overflow-hidden">
       <Hero />
+      <ScrollToTopButton />
     </main>
   )
 }
