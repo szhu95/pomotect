49178e5f2e7c1c5065c2786b9d6a211f81fe4cac
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-02-01 14:46:01 -0800

    standardizing fonts for body of page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -15,50 +15,50 @@ const About = () => {
                     <p><i>Most recently updated on {lastUpdatedDate}</i></p>
                 </div>
                 <div className="about-section">
-                    <h3 className="about_header">Postmodern Tectonics </h3>
+                    <h3 className="about_header minion-font">Postmodern Tectonics </h3>
 
-                    <p>Or, sometimes <b>POMO—TECT™</b> is a b2b (back-to-back) of ideas.</p>
+                    <div className="about-section">Or, sometimes <b>POMO—TECT™</b> is a b2b (back-to-back) of ideas.</div>
 
-                    <p>We talk about, research, and design tools and symbols for people who like to:</p>
+                    <div className="about-section">We talk about, research, and design tools and symbols for people who like to:</div>
 
 
                     <ul className="padding-x">
-                        <li>DJ.</li>
-                        <li>Design.</li>
-                        <li>Dance.</li>
-                        <li>Make Music.</li>
-                        <li>and Art.</li>
-                        <li>Engineer Things.</li>
-                        <li>or Architect Stuff.</li>
+                        <li className="minion-font">DJ.</li>
+                        <li className="minion-font">Design.</li>
+                        <li className="minion-font">Dance.</li>
+                        <li className="minion-font">Make Music.</li>
+                        <li className="minion-font">and Art.</li>
+                        <li className="minion-font">Engineer Things.</li>
+                        <li className="minion-font">or Architect Stuff.</li>
                     </ul>
                     <br />
 
-                    <p>We are eternally inspired by these people, so we outfit them for purpose with our products and celebrate them through our brand.</p>
+                    <div className="about-section">We are eternally inspired by these people, so we outfit them for purpose with our products and celebrate them through our brand.</div>
                 </div>
                 <div className="about-section">
-                    <h3><b>Objects</b></h3>
+                    <div className="minion-font font-bold">Objects</div>
                     Form and function and all that. A collection of things and garments that we make and sometimes
                     sell if we can figure out how to actually make them. Let us know if you want to help with that last part.
                     It’s hard.
                 </div>
 
                 <div className="about-section">
-                    <h3><b>Words</b></h3>
+                    <div className="minion-font font-bold">Words</div>
                     Here we explore general curiosities and questions. We quote. We clip things from the web.
                     Disciplined observation of the world is the source of all creative inspiration so we try
                     to keep an eye or ear out for goings-on worth capturing.
                 </div>
 
                 <div className="about-section">
-                    <h3><b>Events</b></h3>
+                    <div className="minion-font font-bold">Events</div>
                     From time-to-time, we bring people together to dance, to listen more,
                     and to talk less — a temporary autonomous zone.
                 </div>
 
                 <div className="about-section">
-                    <h3 className="about_header">About the About</h3>
-                    Founded in 2024© by a few longtime friends.
-                    <p>Find us somewhere in between New York & Los Angeles.</p>
+                    <h3 className="about_header minion-font ">About the About</h3>
+                    Founded in 2024© by a few longtime friends. <br></br>
+                    Find us somewhere in between New York & Los Angeles.
                 </div>
             </div>
             <ScrollToTopButton />
