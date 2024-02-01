18000e1c310a5693485d4bf21df8dd23bfa23f52
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-02-01 15:26:51 -0800

    updating layout so all buttons appear on load

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,21 +12,21 @@ const Hero = () => {
     return (
         <div className="hero">
             <div className="relative flex-1 pt-20 padding-x landing_page">
-                <div className="absolute right-[0px] top-[35%]">
+                <div className="absolute right-[0px] top-[25%]">
                 <CustomButton
                     title="Explore Objects"
                     containerStyles="bg-primary-blue italic"
                     handleClick={() => router.push('/objects')}
                 />
                 </div>
-                <div className="absolute left-[15%] top-[57%]">
+                <div className="absolute left-[15%] top-[37%]">
                 <CustomButton
                     title="Explore Words"
                     containerStyles="bg-primary-blue italic"
                     handleClick={() => router.push('/words')}
                 />
                 </div>
-                <div className="absolute left-[30%] md:left-[50%] top-[80%]">
+                <div className="absolute left-[30%] md:left-[50%] top-[55%]">
                 <CustomButton
                     title="Explore Sounds"
                     containerStyles="bg-primary-blue italic"
