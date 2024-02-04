38bd2340c366d29eefc87426dc33da82e559c1d3
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-02-03 23:00:43 -0800

    centering icon for cart

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -175,14 +175,14 @@ export default function Cart() {
               </div>
             )
           }) :
-            <svg
-              className={"animate-spin h-5 w-5 m-3"}
-              viewBox={"0 0 24 24"}
-              fill={"none"}
-            >
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"></circle>
-              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
+              <svg
+                className={"block m-auto my-6 animate-spin h-5 w-5"}
+                viewBox={"0 0 24 24"}
+                fill={"none"}
+              >
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
           )
       }
       <div className="text-right pr-2 font-semibold pb-2 italic border-b-2 border-terracotta font-['Minion']">TOTAL BEFORE TAXES + SHIPPING</div>
