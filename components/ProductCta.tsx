c5cc9a614e63ff2a6083b8c0ccd9b40fbb130977
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-02-04 15:29:34 -0800

    changing button color to blue

--- components/ProductCta.tsx
+++ components/ProductCta.tsx
@@ -146,7 +146,7 @@ const ProductCta = ({ variantName, options, quantity, variants }: any) => {
     <div>
       <CustomDropdown selected={size} title={variantName} options={options} handleChange={setSize} />
 
-      {isLoading == false ? quantity == "0" ? <div className="mt-5 minion-font italic border-dashed border border-slate-400 text-center">SOLD OUT</div> : <CustomButton containerStyles="w-full group hover:bg-primary-blue border border-2 border-primary-blue text-black font-medium mt-5 minion-font" textColor="group-hover:text-white minion-font" title={"ADD TO CART"} handleClick={() => mapVariants(variantArr, size)} /> : <LoadButton />}
+      {isLoading == false ? quantity == "0" ? <div className="mt-5 minion-font italic border-dashed border border-slate-400 text-center">SOLD OUT</div> : <CustomButton containerStyles="w-full group hover:bg-primary-blue border border-2 border-primary-blue font-medium mt-5 minion-font" textColor="group-hover:text-white minion-font text-primary-blue" title={"ADD TO CART"} handleClick={() => mapVariants(variantArr, size)} /> : <LoadButton />}
       {itemAdded ?
         <Link href='/cart'>
           <div className="mt-4 text-center minion-font animate-pulse border border-primary-blue border-dashed text-primary-blue transition-opacity ease-in-out duration-700 opacity-100">
