87cf2589677ff0666d56de202805df58c9a35974
Stevie Hu <stephenhu@Stephens-MacBook-Pro.local>
2024-02-23 12:17:12 -0800

    adding animation to reset link

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return <div className="text-center italic ">
       <h1 className='minion-font mt-20'>404: Navigation out of sync . . .</h1>
       <div className='mt-10'>
-        <Link href="/" className="minion-font text-purple">Reset Initial Cue Point</Link>
+        <Link href="/" className="minion-font text-purple focus:bg-black focus:text-white hover:bg-black hover:text-white">Reset Initial Cue Point</Link>
       </div>
   </div>
 }
