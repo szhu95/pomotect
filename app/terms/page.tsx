6fd58ee422d0ff325957714d0ac6cdfc581a2b3a
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-02-26 12:27:55 -0800

    debugging nesting iframe in div

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -7,10 +7,7 @@ const Terms = () => {
         <div>
             <div className="site-section">
                 <div className="about-section">
-                    <object
-                        className="border-2 border-primary-blue border-dashed w-full h-[100vh]"
-                        data="terms.html"
-                        type="text/html"></object>
+                    <iframe className="border-2 border-primary-blue border-dashed w-full h-[100vh]" src="terms.html"></iframe>
                 </div>
             </div>
             <ScrollToTopButton />
