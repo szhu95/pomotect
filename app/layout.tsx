f8d3dedffe8d17b2a83dde020a3718ee93d52788
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-02-29 11:19:28 -0800

    adding background image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Footer, Header } from '@/components'
+import bg from '../assets/images/bg.jpeg'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,10 +17,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  
+
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className} style={{
+        backgroundImage: `url(${bg.src})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '50%',
+        backgroundAttachment: 'fixed',
+        backgroundPosition: 'bottom 10% right 0px',
+        objectFit: 'cover'
+      }}>
         <div className="site-layout">
           <Header />
           {children}
