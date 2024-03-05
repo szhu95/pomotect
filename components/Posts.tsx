02326cc84d1ff3ebfe7e3853b7bb0212a17bccfc
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-03-04 18:28:25 -0800

    formatting words page

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -31,9 +31,9 @@ export default async function Posts({ response }: any) {
                         <div className="entry-number bg-black text-white font-['Minion']">{post.title ? post.title : 'Title'}</div>
                         <div className="font-['Minion'] italic">On {formattedPostDate}, {(post.primary_author.name ? post.primary_author.name : "Anonymous")} {'<' + (post.custom_excerpt ? post.custom_excerpt : "office@pomotect.com") + '>'} wrote:</div>
                     </div>
-                    <div className="site-section ml-2 words-body max-h-96 overflow-y-auto">
+                    <div className="site-section ml-2 words-body max-h-[85vh] overflow-y-auto">
                         <div className="pr-2 py-1 font-['Minion'] text-justify">{parsedPost}</div>
-                        <div className="pr-2 py-4">
+                        <div className="pr-2 py-2">
                             {post.feature_image && <Image
                                 src={post.feature_image}
                                 alt={"words-image"}
