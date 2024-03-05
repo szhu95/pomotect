a58cd0f10143e0c0d883921d136d5ed52e32b629
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-03-04 17:54:43 -0800

    fixing styling for flyers

--- app/sounds/events/page.tsx
+++ app/sounds/events/page.tsx
@@ -21,19 +21,20 @@ const Events = () => {
                 <Link href="/sounds/events" scroll={false} className="objects_link bg-black text-white hover:bg-black hover:text-white">Events</Link>
             </div>
             <div className="flex px-2 w-1/8 max-w-12 site-section">
-                <ol className="relative">
+                <ol className="w-full">
                     <div className="text-right text-primary-blue italic font-semibold font-['Minion'] border-b border-dashed border-primary-blue -ml-2">UPCOMING</div>
                     <li className="border-b border-black -ml-2">
-                        <span className="soundcloud_title ml-2 font-['Minion']">Flash</span>
+                        <span className="soundcloud_title ml-2 font-['Minion']">Flash Nightclub</span>
                         <br></br>
                         <p className="text-right text-primary-blue italic font-['Minion']">Flash, Washington D.C.</p>
                     </li>
-                    <div className="mt-3 max-w-10 flex-col items-center center centered">
+                    <div className="mt-4 max-w-10 flex-col">
                         <Image
-                            width={1000}
-                            height={1000}
+                            width={650}
+                            height={650}
                             src={FlashFlyer}
                             alt="Flash Flyer"
+                            className='mx-auto'
                         />
                     </div>
                     <div className="text-right text-grey italic font-semibold font-['Minion'] mt-4 border-b border-grey border-dashed -ml-2">ARCHIVE</div>
@@ -42,12 +43,13 @@ const Events = () => {
                         <br></br>
                         <p className="text-right text-primary-blue italic font-['Minion']">Winston House, Los Angeles</p>
                     </li>
-                    <div className="mt-3 max-w-10 flex-col items-center center centered">
+                    <div className="mt-4 min-w-10 flex-col">
                         <Image
-                            width={1000}
-                            height={1000}
+                            width={650}
+                            height={650}
                             src={WinstonFlyer}
                             alt="Winston Flyer"
+                            className='mx-auto'
                         />
                     </div>
                 </ol>
