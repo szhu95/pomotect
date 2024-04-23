84eec5057faa6f8f7a2e96c2a12116d0db1f1f6a
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-04-23 14:39:16 -0700

    adding active link highlight for navigation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,18 @@
+"use client";
 import BlueHandLogo from "../assets/images/blue-hand-logo.png"
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from "next/navigation";
 
 const Footer = () => {
-
+    const pathname = usePathname();
     return (
         <div className="flex border-t border-black mt-[250px]">
             <div className="site-section italic text-gray-700 py-1">
-                <Link href="/terms" className="mr-8 pr-2 md:mr-10 hover:bg-primary-blue hover:text-white">
+                <Link href="/terms" className={pathname.startsWith("/terms") ? "mr-8 pr-2 md:mr-10 bg-primary-blue text-white hover:bg-primary-blue hover:text-white" : "mr-8 pr-2 md:mr-10 hover:bg-primary-blue hover:text-white"}>
                     Privacy & Terms of Use
                 </Link>
-                <Link href="/contact" className="hover:bg-primary-blue hover:text-white px-2">
+                <Link href="/contact" className={pathname.startsWith("/contact") ? "bg-primary-blue text-white hover:bg-primary-blue hover:text-white px-2" : "hover:bg-primary-blue hover:text-white px-2"}>
                     Contact
                 </Link>
             </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,12 @@ import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import homeLogo from '../assets/images/logo-3.png'
 import { motion } from 'framer-motion';
+import { usePathname } from 'next/navigation';
 
 const Header = ({ title, menuStatus }: any) => {
     const [cartFilled, setCartFilled] = useState(false)
     const [menuIcon, setIcon] = useState(false);
+    const pathname = usePathname();
 
     const handleSmallerScreensNavigation = () => {
         setIcon(!menuIcon);
@@ -24,29 +26,29 @@ const Header = ({ title, menuStatus }: any) => {
         <div className="padding-y">
             <div onClick={() => setIcon(false)} className="home-link mb-4">
                 <Link href="/" className="padding-y">
-                        <Image
-                            src={homeLogo}
-                            priority
-                            width={650}
-                            height={600}
-                            alt="home page link"
-                            className="w-auto h-auto"
-                        />
+                    <Image
+                        src={homeLogo}
+                        priority
+                        width={650}
+                        height={600}
+                        alt="home page link"
+                        className="w-auto h-auto"
+                    />
                 </Link>
             </div>
             <div className="header-link hidden md:block">
-                <Link href="/about" scroll={false} className="hover:bg-primary-blue px-2 hover:text-white">About</Link>
-                <Link href="/objects" scroll={false} className="margin-x px-2 hover:bg-primary-blue hover:text-white">Objects</Link>
-                <Link href="/words" scroll={false} className="margin-x px-2 hover:bg-primary-blue hover:text-white">Words</Link>
-                <Link href="/sounds" scroll={false} className="margin-x px-2 hover:bg-primary-blue hover:text-white">Sounds</Link>
-                <Link href="/cart" scroll={false} id="checkout-btn" className="float-right px-2 mr-2 hover:bg-terracotta hover:text-white">[ Cart ]</Link>
+                <Link href="/about" scroll={false} className={pathname.startsWith("/about") ? "bg-primary-blue text-white hover:bg-primary-blue px-2 hover:text-white" : "hover:bg-primary-blue px-2 hover:text-white"}>About</Link>
+                <Link href="/objects" scroll={false} className={pathname.startsWith("/objects")  ? "margin-x px-2 bg-primary-blue text-white hover:bg-primary-blue hover:text-white" : "margin-x px-2 hover:bg-primary-blue hover:text-white"}>Objects</Link>
+                <Link href="/words" scroll={false} className={pathname.startsWith("/words") ? "margin-x px-2 bg-primary-blue text-white hover:bg-primary-blue hover:text-white" : "margin-x px-2 hover:bg-primary-blue hover:text-white"}>Words</Link>
+                <Link href="/sounds" scroll={false} className={pathname.startsWith("/sounds") ? "margin-x px-2 bg-primary-blue text-white hover:bg-primary-blue hover:text-white" : "margin-x px-2 hover:bg-primary-blue hover:text-white"}>Sounds</Link>
+                <Link href="/cart" scroll={false} id="checkout-btn" className={pathname.startsWith("/cart") ? "float-right bg-terracotta text-white px-2 mr-2 hover:bg-terracotta hover:text-white" : "float-right px-2 mr-2 hover:bg-terracotta hover:text-white"}>[ Cart ]</Link>
             </div>
             <div className="header-link flex md:hidden cursor-pointer">
                 <div className="hover:bg-primary-blue hover:text-white" onClick={handleSmallerScreensNavigation}>
                     {menuIcon ? <div className="not-italic focus:text-white hover:text-white">ⓧ</div> : "Menu"}
                 </div>
                 <div className="checkout_btn" onClick={() => setIcon(false)} >
-                    <Link href="/cart" scroll={false} id="checkout-btn-mobile" className="px-2 mr-2 hover:bg-terracotta hover:text-white">[ Cart ]</Link>
+                    <Link href="/cart" scroll={false} id="checkout-btn-mobile" className={pathname.startsWith("/cart") ? "px-2 mr-2 bg-terracotta text-white hover:bg-terracotta hover:text-white" : "px-2 mr-2 hover:bg-terracotta hover:text-white"}>[ Cart ]</Link>
                 </div>
             </div>
 
@@ -59,19 +61,19 @@ const Header = ({ title, menuStatus }: any) => {
                 <div className="w-full border-b border-black">
                     <ul>
                         <li onClick={handleSmallerScreensNavigation} className="py-2 cursor-pointer text-black">
-                            <Link href="/about" scroll={false} className=" hover:bg-primary-blue hover:text-white px-2">About</Link>
+                            <Link href="/about" scroll={false} className={pathname.startsWith("/about") ? "bg-primary-blue text-white hover:bg-primary-blue hover:text-white px-2" : "hover:bg-primary-blue hover:text-white px-2"}>About</Link>
                         </li>
 
                         <li onClick={handleSmallerScreensNavigation} className="py-2 cursor-pointer text-black">
-                            <Link href="/objects" scroll={false} className=" hover:bg-primary-blue hover:text-white px-2">Objects</Link>
+                            <Link href="/objects" scroll={false} className={pathname.startsWith("/objects") ? "bg-primary-blue text-white hover:bg-primary-blue hover:text-white px-2" : "hover:bg-primary-blue hover:text-white px-2"}>Objects</Link>
                         </li>
 
                         <li onClick={handleSmallerScreensNavigation} className="py-2 cursor-pointer text-black">
-                            <Link href="/words" scroll={false} className=" hover:bg-primary-blue hover:text-white px-2">Words</Link>
+                            <Link href="/words" scroll={false} className={pathname.startsWith("/words") ? "bg-primary-blue text-white hover:bg-primary-blue hover:text-white px-2" : "hover:bg-primary-blue hover:text-white px-2"}>Words</Link>
                         </li>
 
                         <li onClick={handleSmallerScreensNavigation} className="py-2 cursor-pointer text-black">
-                            <Link href="/sounds" scroll={false} className=" hover:bg-primary-blue hover:text-white px-2">Sounds</Link>
+                            <Link href="/sounds" scroll={false} className={pathname.startsWith("/sounds") ? "bg-primary-blue text-white hover:bg-primary-blue hover:text-white px-2" : "hover:bg-primary-blue hover:text-white px-2"}>Sounds</Link>
                         </li>
                     </ul>
                 </div>
