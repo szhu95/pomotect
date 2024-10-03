80364bdaff9dee371a7f35f74edf1949b3d8d8d5
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-10-02 18:10:02 -0700

    updating animation size

--- app/loading.js
+++ app/loading.js
@@ -5,7 +5,7 @@ import globeAnimation from '../assets/images/globe-animation.gif'
 export default function Loading() {
     return (
         <div className="fixed inset-0 bg-white z-[10000] hidden md:flex flex-1 items-center justify-center">
-            <Image src={globeAnimation} alt="my gif" height={500} width={500} />
+            <Image src={globeAnimation} alt="pomo-globe" height={300} width={300} />
         </div>
     );
 }
