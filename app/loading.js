aab6da5f6dabc13789501ed29e19b068fd8f7a05
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-10-02 18:17:03 -0700

    adding priority tag

--- app/loading.js
+++ app/loading.js
@@ -5,7 +5,13 @@ import globeAnimation from '../assets/images/globe-animation.gif'
 export default function Loading() {
     return (
         <div className="fixed inset-0 bg-white z-[10000] hidden md:flex flex-1 items-center justify-center">
-            <Image src={globeAnimation} alt="pomo-globe" height={300} width={300} />
+            <Image
+                src={globeAnimation}
+                alt="pomo-globe"
+                height={300}
+                width={300}
+                priority
+            />
         </div>
     );
 }
