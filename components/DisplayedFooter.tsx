3c9765482f2198c0a61c056e28c6ee0ae0338082
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-10-31 16:46:24 -0700

    adding instagram easter egg

--- components/DisplayedFooter.tsx
+++ components/DisplayedFooter.tsx
@@ -1,5 +1,6 @@
 "use client";
 import InstaLogo from "../assets/images/insta-logo.png"
+import InstaEasterLogo from "../assets/images/insta-easter-logo.png"
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from "next/navigation";
@@ -7,7 +8,7 @@ import { WordsTicker } from "@/components";
 import localFont from 'next/font/local';
 
 const pomotectFont = localFont({
-  src: '../fonts/pomotect-analog-regular.otf',
+    src: '../fonts/pomotect-analog-regular.otf',
 });
 
 
@@ -16,7 +17,8 @@ const DisplayedFooter = ({ response, objectsResponse }: any) => {
     return (
         <div>
             {pathname == "/" || pathname == "/cart" ? <></> : <WordsTicker response={response} objectsResponse={objectsResponse} />}
-            <div className={pathname == "/" ? "flex border-t border-black md:mt-[75%] mt-[140%]" : pathname == "/cart" ? "flex border-t border-black mt-[250px]" : "flex border-t border-black mt-[10px]"}>
+            <div className={
+                pathname == "/" ? "flex border-t border-black md:mt-[75%] mt-[140%]" : pathname == "/cart" ? "flex border-t border-black mt-[250px]" : "flex border-t border-black mt-[10px]"}>
                 <div className="site-section italic text-gray-700 py-1">
                     <Link href="/terms" className={`${pomotectFont.className} ${pathname.startsWith("/terms") ? "pr-2 md:mr-10 text-primary-blue hover:bg-primary-blue hover:text-white" : "pr-2 md:mr-10 hover:bg-primary-blue hover:text-white"}`}>
                         Privacy & Terms of Use
@@ -26,14 +28,25 @@ const DisplayedFooter = ({ response, objectsResponse }: any) => {
                     </Link>
                 </div>
                 <div className="site-section italic pt-1 text-primary-blue checkout_btn">
-                    <a target="_blank" href="https://www.instagram.com/pomotect/" rel="noopener noreferrer">
-                        <Image
-                            src={InstaLogo}
-                            width={18}
-                            height={18}
-                            alt="postmodern tectonics instagram"
-                        />
-                    </a>
+                    {
+                        pathname == "/cart" ?
+                            <a target="_blank" href="https://www.instagram.com/pomotect/" rel="noopener noreferrer">
+                                <Image
+                                    src={InstaEasterLogo}
+                                    width={18}
+                                    height={18}
+                                    alt="postmodern tectonics instagram"
+                                />
+                            </a> :
+                            <a target="_blank" href="https://www.instagram.com/pomotect/" rel="noopener noreferrer">
+                                <Image
+                                    src={InstaLogo}
+                                    width={18}
+                                    height={18}
+                                    alt="postmodern tectonics instagram"
+                                />
+                            </a>
+                    }
                 </div>
             </div>
         </div>
