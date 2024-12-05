0dcb92261ee966c6b125115b495158df253ffd2a
Stevie Hu <stephenhu@Stephens-MBP.lan>
2024-12-05 12:15:56 -0800

    updating sharp package

--- components/Post.tsx
+++ components/Post.tsx
@@ -8,7 +8,7 @@ export default async function Post({ response }: any) {
     return (
         response.posts.map((post: any) => {
 
-            //console.log("POST IS +++++" + JSON.stringify(post))
+            // console.log("POST IS +++++" + JSON.stringify(post))
             let postUtcTimeString = post.published_at;
             let postDate = new Date(postUtcTimeString)
             let formattedPostDate = formatUpdatedDate(postDate);
