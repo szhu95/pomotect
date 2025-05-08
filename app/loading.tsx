10e241078b37c152e3a40a520aab290c18d2513b
Stevie Hu <stephenhu@Stephens-MacBook-Pro.local>
2025-05-08 00:58:22 -0700

    optimizing gif

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,18 +1,18 @@
 "use client"
 import React from "react";
-import Image from 'next/image';
-import globeAnimation from '../assets/images/globe-animation.gif'
 
 export default function Loading() {
     return (
         <div className="fixed inset-0 bg-white z-[10000] hidden md:flex flex-1 items-center justify-center">
-            <Image
-                src={globeAnimation}
-                alt="pomo-globe"
-                height={300}
+            <video
+                src="/globe-animation.webm"
+                autoPlay
+                loop
+                muted
+                playsInline
                 width={300}
-                priority
-                unoptimized
+                height={300}
+                className="w-[300px] h-[300px]"
             />
         </div>
     );
